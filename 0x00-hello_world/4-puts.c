1da6e68210504e1e88584bc57ff4e6833bf76c01
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-09 15:47:50 -0700

    x

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 /**
  *main - entry point
  *
- *Description: a function of c to 
+ *Description: a function of c to
  *
  *Return: always 0 (success)
 */
 #include <stdio.h>
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0; 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
