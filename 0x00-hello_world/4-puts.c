6acade16189f172f6c5b42e08cea976cda28f1e5
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-09 15:39:41 -0700

    x

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ *main - entry point
+ *
+ *Description: a function of c to 
+ *
+ *Return: always 0 (success)
+*/
+#include <stdio.h>
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0; 
+}
