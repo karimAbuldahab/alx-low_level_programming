f9b9d95db97c386f5e3e73e66f8fb4cfb1ddc969
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-09 15:55:07 -0700

    x

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c5-printf.c
@@ -0,0 +1,13 @@
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
