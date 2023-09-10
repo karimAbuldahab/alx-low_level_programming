b8d3ebb3fbc084fa0b35bab20681d9aa36f6969a
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-10 13:48:22 -0700

    x

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 1 (not success)
+*/
+#include <unistd.h>
+int main(void)
+{
+char  text[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1,text,59)
+	return (1);
+}
