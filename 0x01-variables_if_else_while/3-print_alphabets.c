cb40b8c1c44379eb4e5cc90501e145e8dfb6d29c
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-15 07:45:09 -0700

    x

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+	for (x = 'A'; x <= 'Z'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
+
