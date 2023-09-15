90a8d3e0416f273abc3c7aa387a961fafd6b61f4
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-15 09:17:23 -0700

    x

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{putchar(x); }
+putchar('\n');
+return (0);
+}
+
