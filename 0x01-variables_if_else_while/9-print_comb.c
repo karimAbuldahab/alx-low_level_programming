87b26ad2a10462ea1f107bb82c6f7e98865fc2f4
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-15 14:56:01 -0700

    x

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 48; x <= 57; x++)
+	{
+	putchar(x);
+	if (x < 57)
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+}
