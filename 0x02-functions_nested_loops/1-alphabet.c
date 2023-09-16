e4b15675f83487502efadc760d6bc22dcf1961fa
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-16 10:47:14 -0700

    x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ *main - entry point
+ *
+ *Description: a function of c to
+ *
+ *Return: always 0 (success)
+*/
 void print_alphabet(void)
 {
 	int x;
