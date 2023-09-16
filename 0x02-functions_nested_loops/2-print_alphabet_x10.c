74482b07a5d2d45f50741d90b5b996ddb83d270c
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-16 10:59:21 -0700

    x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - entry point
+ *
+ *Description: a function of c to print alphabet
+ *
+ *Return: always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+	int a, x;
+
+	for (a=0; x < 9; a++)
+	{
+	for (x = 'a'; x <= 'z'; x++)
+	{_putchar(x); }
+	_putchar('\n');
+	}
+}
