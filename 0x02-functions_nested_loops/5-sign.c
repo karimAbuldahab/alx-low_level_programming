9caf49ffd6e477a311ae9e3c70bc65447904397e
karimAbuldahab <karim.abuldahab@gmail.com>
2023-09-20 12:47:35 -0700

    x

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_isalpha - entry point
+ *
+ *@c: a function of c to check lower
+ *
+ *Return: 0 or 1 (success)
+*/
+int print_sign(int n);
+{
+	if (n >= 48)
+	{
+		if (n =48)
+			{_putchar(48);
+			return (0); }
+		_putchar(43);
+		return (1);
+	}
+	_putchar(45);
+	return (0);
+}
